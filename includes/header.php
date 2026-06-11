909cb21e9348b44f0cb4c04917bbf69213bd4e65
Neo_cdes <144217975+NEO-SEHLOHO@users.noreply.github.com>
2026-06-11 11:22:01 +0200

    Delete includes directory

--- includes/header.php
+++
@@ -1,5 +0,0 @@
-﻿<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo htmlspecialchars($pageTitle, ENT_QUOTES, 'UTF-8'); ?></title>
-<meta name="description" content="Branding, design and printing services including banners, flags, displays, and promotional materials.">
-<link rel="stylesheet" href="assets/css/style.css">
